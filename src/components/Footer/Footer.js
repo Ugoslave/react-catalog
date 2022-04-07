06ac0c8b605a030cc60b70b792bd3a300a0e02b2
Alexey Peresypkin <ugo200@yandex.ru>
2022-04-07 19:33:29 +0500

    done: footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer__copyright">&copy; {`${new Date().getFullYear()} Rates & Routes Russia`}</p>
+      <p className="footer__copyright">&copy; {`${new Date().getFullYear()} Explorer Russia`}</p>
     </footer>
   );
 }
